1b8e6f179c329e0a60232360c925d5e076ecfa57
vongani <vonganikhulufelo@gmail.com>
2018-05-19 04:35:06 +1400

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,16 +49,16 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'https://learningstyles.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :authentication => 'plain',
+    :user_name      => 'nname4440@gmail.com',
+    :password       => '17932468',
+    :enable_starttls_auto => true,
+    :domain         => 'gmail.com',
   }
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
